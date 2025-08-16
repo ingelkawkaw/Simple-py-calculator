206578ff52d9a84b566d999bb027e7a80da7f5c6
angel grueso <149785576+ingelkawkaw@users.noreply.github.com>
2025-08-16 15:13:55 +0800

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -21,8 +21,9 @@ while True: #program will loop until user exits
     print("[2] Subtraction")
     print("[3] Multiplication")
     print("[4] Division")
-    print("[5] Square root")
-    print("[6] Exit")
+    print ("[5] Modulus")
+    print("[6] Square root")
+    print("[7] Exit")
     print()
 
     print("Select an action that you would like to perform:")
@@ -53,19 +54,31 @@ while True: #program will loop until user exits
         if (num2 != 0):
             print()
             print (GREEN + "Answer:" , num1 / num2, RESET)
+            print (GREEN + "Round down:" , math.floor(num1 / num2), RESET)
+            print (GREEN + "Round up:" , math.ceil(num1 / num2), RESET)
+
         else:
             print()
             print (RED + "You can't divide by 0 bozo" + RESET)
             print(PURPLE + "Try again :)))" + RESET)
 
+    
     elif(UserInput == "5"):
+        print("Please enter the numbers separated by a comma")
+        num1, num2 = map(int, input().split(','))
+        print()
+        print (GREEN + "Answer:" , num1 % num2, RESET)
+
+    elif(UserInput == "6"):
         print("Please enter the number")
         num = (float(input()))
         result = math.sqrt(num)
         print()
         print( GREEN + "Answer:" , result , RESET)
+        print (GREEN + "Round down:" , math.floor(result), RESET)
+        print (GREEN + "Round up:" , math.ceil(result), RESET)
 
-    elif(UserInput == "6"):
+    elif(UserInput == "7"):
         print()
         print(PURPLE + "Exiting..." + RESET)
         break
