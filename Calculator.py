be39df65fdea7e326252c13eb031e8204184ef70
angel grueso <149785576+ingelkawkaw@users.noreply.github.com>
2025-08-16 14:57:32 +0800

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,75 @@
+#Simple calculator lez gooooo
+
+#colors yeheyyyyy
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+PURPLE = '\033[95m'
+CYAN = '\033[96m'
+WHITE = '\033[97m'
+RESET = '\033[0m'
+
+import math #for sqrt
+while True: #program will loop until user exits 
+
+    line = "---------------------------------------------------"
+    print(line)
+    print(BLUE + "                    <Calculator>" + RESET)
+    print()
+    print("[1] Addition")
+    print("[2] Subtraction")
+    print("[3] Multiplication")
+    print("[4] Division")
+    print("[5] Square root")
+    print("[6] Exit")
+    print()
+
+    print("Select an action that you would like to perform:")
+    UserInput = input ()
+    print(line)
+
+    if (UserInput == "1"):
+        print("Please enter the numbers separated by a comma")
+        num1, num2 = map(int, input().split(','))
+        print()
+        print (GREEN + "Answer:" , num1 + num2, RESET)
+
+    elif(UserInput == "2"):
+        print("Please enter the numbers separated by a comma")
+        num1, num2 = map(int, input().split(','))
+        print()
+        print (GREEN + "Answer:" , num1 - num2, RESET)
+
+    elif(UserInput == "3"):
+        print("Please enter the numbers separated by a comma")
+        num1, num2 = map(int, input().split(','))
+        print()
+        print (GREEN + "Answer:" , num1 * num2, RESET)
+
+    elif(UserInput == "4"):
+        print("Please enter the numbers separated by a comma")
+        num1, num2 = map(int, input().split(','))
+        if (num2 != 0):
+            print()
+            print (GREEN + "Answer:" , num1 / num2, RESET)
+        else:
+            print()
+            print (RED + "You can't divide by 0 bozo" + RESET)
+            print(PURPLE + "Try again :)))" + RESET)
+
+    elif(UserInput == "5"):
+        print("Please enter the number")
+        num = (float(input()))
+        result = math.sqrt(num)
+        print()
+        print( GREEN + "Answer:" , result , RESET)
+
+    elif(UserInput == "6"):
+        print()
+        print(PURPLE + "Exiting..." + RESET)
+        break
+
+    else:
+        print()
+        print (RED + "Bro....please choose a number from 1-6" + RESET)
